8a6a0617f9169c2fa6c1f92a9b0fc9aafeddab2a
bbth <bbth@acm.org>
2020-10-12 15:24:17 -0400

    first commit

---
+++ plot_MI.R
@@ -0,0 +1,25 @@
+plot_MI <- function(df, min_MI = 0.5) {
+  require(tidyverse)
+  require(tidygraph)
+  require(ggraph)
+  
+  temp <- df %>% 
+    filter(MI >= min_MI) %>%
+    mutate(from = Position_1, to = Position_2) %>%
+    select(from, to, MI)
+  
+  mi_graph <- as_tbl_graph(temp, directed = FALSE)
+  
+  p <- mi_graph %>% 
+        ggraph(layout = 'kk') + 
+        geom_edge_link() +
+        geom_node_point(size = 20, color = 'steelblue') +
+        geom_node_text(aes(label = name), color = 'white') +
+        geom_edge_link(aes(edge_color = cut_interval(MI, 10))) +
+        guides(edge_color = guide_legend(title = 'MI')) +
+        ggtitle('MI Graph') +
+        theme_graph(base_family = 'sans')
+  
+  print(p)
+}
+  
