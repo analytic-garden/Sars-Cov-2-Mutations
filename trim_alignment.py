d3d43e6bcd1b96e259c976889c5226700c82c2f1
bbth <bbth@acm.org>
2020-10-14 10:31:58 -0400

    updated file location

--- trim_alignment.py
+++ trim_alignment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-trim_alignment_py
+trim_alignment_py - remove low quality regions from either end of alignment
 
 @author: Bill Thompson
 @license: GPL 3
@@ -12,13 +12,14 @@ from Bio import AlignIO
 from consensus3 import get_col_range
 
 def main():
-    path = '/mnt/g/Covid-19/2020_08_28/'
-    
-    align_file = path + 'sequences_filtered_aln.fasta'
-    out_file = path + 'sequences_filtered_aln_red.fasta'
+    date = '2020_10_07'
+    base = '/mnt/g/Covid-19/' + date + '/' 
+     
+    align_file = base + 'sequences_filtered_aln.fasta'
+    out_file = base + 'sequences_filtered_aln_red.fasta'
     
     align = AlignIO.read(open(align_file), 'fasta')
-    start, end = get_col_range(align, min_quality = 0.99)
+    start, end = get_col_range(align, min_quality = 0.90)
     
     align2 = align[:, start:(end+1)]
     AlignIO.write(align2, open(out_file, 'w'), 'fasta')
