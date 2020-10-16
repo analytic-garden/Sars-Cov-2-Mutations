8cf032b8fd026666f6604a4d8cdc37d79634c4be
bbth <bbth@acm.org>
2020-10-16 10:26:08 -0400

    updated to make more generic

--- plot_tree.R
+++ plot_tree.R
@@ -2,7 +2,9 @@ plot_tree <- function(tree, df) {
   require(tidyverse)
   require(ggtree)
   
-  p <- ggplot(tree.samp4, aes(x,y)) + geom_tree() + theme_tree() 
+  # %<+% is a ggtree operator to add annotation data to a  tree
+  
+  p <- ggplot(tree, aes(x,y)) + geom_tree() + theme_tree() 
 #  p %<+% df + geom_tiplab(aes(color=tag))
   p %<+% df + geom_tippoint(aes(color=tag, shape=usa))
 }
