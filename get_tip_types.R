9939aea307da9900f5f9bdce3ffe82fe6351b094
bbth <bbth@acm.org>
2020-10-16 10:26:01 -0400

    updated to make more generic

--- get_tip_types.R
+++ get_tip_types.R
@@ -1,7 +1,7 @@
 get_tip_types <- function(tree) {
   require(tidyverse)
   
-  tips <- tree.samp4$tip.label
+  tips <- tree$tip.label
   df <- data.frame(seq = tips, 
                    tag = as.factor(str_extract(tips, '.-.-.-.')),
                    usa = ! is.na(str_extract(tips, 'USA')))
