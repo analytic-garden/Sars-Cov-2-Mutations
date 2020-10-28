269672da040bf92dbd8dcb384c58983a1845c77c
bbth <bbth@acm.org>
2020-10-28 10:57:59 -0400

    changed color cuts

--- plot_MI.R
+++ plot_MI.R
@@ -20,7 +20,7 @@ plot_MI <- function(df, min_MI = 0.5) {
         geom_edge_link() +
         geom_node_point(size = 20, color = 'steelblue') +
         geom_node_text(aes(label = name), color = 'white') +
-        geom_edge_link(aes(edge_color = cut_interval(MI, 10))) +
+        geom_edge_link(aes(edge_color = cut_interval(MI, 8))) +
         guides(edge_color = guide_legend(title = 'MI')) +
         ggtitle('MI Graph') +
         theme_graph(base_family = 'sans')
