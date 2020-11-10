ff6927f873e584faa5870d0723e78513737be203
bbth <bbth@acm.org>
2020-11-10 16:24:15 -0500

    adjusted edge colors

--- plot_MI.R
+++ plot_MI.R
@@ -14,17 +14,16 @@ plot_MI <- function(df, min_MI = 0.5, layout = 'kk') {
     select(from, to, MI)
   
   mi_graph <- as_tbl_graph(temp, directed = FALSE)
-
+  
   p <- mi_graph %>% 
         ggraph(layout = layout) + 
-        geom_edge_link() +
         geom_node_point(size = 20, color = 'steelblue') +
         geom_node_text(aes(label = name), color = 'white') +
-        geom_edge_link(aes(edge_color = cut_interval(MI, 8))) +
+        geom_edge_link(aes(edge_color = cut_interval(MI, 8)), edge_width = 1) +
         guides(edge_color = guide_legend(title = 'MI')) +
         ggtitle('MI Graph') +
-        theme_graph(base_family = 'sans')
-  
+        theme_graph(base_family = 'sans') 
+
   print(p)
 }
   
