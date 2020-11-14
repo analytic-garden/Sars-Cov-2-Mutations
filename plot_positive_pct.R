7594aa071b50389fe5b509634596871d509e3e6f
bbth <bbth@acm.org>
2020-11-14 09:05:58 -0500

    first commit

---
+++ plot_positive_pct.R
@@ -0,0 +1,34 @@
+plot_positive_pct <- function(df, counties = c('Albany', 'Columbia', 'Rensselaer', 'Saratoga', 'Schenectady')) {
+  require(tidyverse)
+  require(zoo)
+  require(rlist)
+  require(grid)
+  require(gridExtra)
+  
+  plot_list <- list()
+  df2 <- data.frame(Test.Date = NULL, County = NULL, Pct = NULL)
+  for(in_county in counties) {
+    temp <- df %>% 
+      filter(County == {{ in_county }}) %>% 
+      mutate(Pct = ifelse(Total.Number.of.Tests.Performed == 0, 0, (New.Positives/Total.Number.of.Tests.Performed) * 100)) %>%
+      select(Test.Date, County, Pct)
+    
+    p2 <- ggplot(temp, aes(x=Test.Date, y=Pct)) + 
+          geom_line() +
+          labs(title = paste('Pct Positive Cases', {{ in_county }}, sep = ' '),
+               y = 'Pct')
+    plot_list <- list.append(plot_list, p2)
+    
+    df2 <- rbind(df2, temp)
+  }
+
+  p <- ggplot(df2, aes(x=Test.Date, y=Pct, color = County)) + 
+      geom_line() +
+      labs(title = 'Pct Positive Cases',
+           y = 'Pct')
+  print(p)
+  
+  grid.arrange(grobs = plot_list, ncol=2, top = textGrob(Sys.Date()))
+  
+  return(df2)
+}
